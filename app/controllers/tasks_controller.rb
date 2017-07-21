9fb1a5334bea6b48734a1281b2e7ee309eb44fee
Gretchen <gretchonian@hotmail.com>
2017-07-21 21:31:59 +0000

    Test

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
   def index
+    #bypasses view/index/erb file. JSON instead of HTML (this is the beginning of removing the refresh step).
     render json: Task.all 
   end
 end
+
