67936f39a38eb6496a2a6c3fa674178ec5c087f4
Gretchen <gretchonian@hotmail.com>
2017-07-26 20:33:06 +0000

    Added Check Fxn

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,5 +3,17 @@ class TasksController < ApplicationController
     #bypasses view/index/erb file. JSON instead of HTML (this is the beginning of removing the refresh step).
     render json: Task.all 
   end
+
+  def update
+    task = Task.find(params[:id])
+    task.update_attributes(task_params)
+    render json: task 
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:done)
+  end
 end
 
